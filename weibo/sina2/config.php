3fdc963f0954eb24236b330405d295e44d4f927c
xiangwenwe <xiangwenwe@foxmail.com>
2013-08-18 23:44:19 +0800

    新浪接口调用的实现
    对新浪微博接口调用的实现，第一版与第二版。

---
+++ weibo/sina2/config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '1645275562' );
+define( "WB_SKEY" , '31d98eb2ef0529c25eb0f6df046a71dd' );
+define( "WB_CALLBACK_URL" , 'http://localhost/weibo/sina/callback.php' );
